0ea8ed0808069e04b5f05965be2eee05e78e3e80
Omars105 <omars105@icloud.com>
2026-08-15 22:33:52 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,10 +5,10 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'omar-dockerhub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push nanatwn/demo-app:jma-2.0'
+        sh 'docker push omar1015/omar-test:jma-2.0'
     }
 }
 
